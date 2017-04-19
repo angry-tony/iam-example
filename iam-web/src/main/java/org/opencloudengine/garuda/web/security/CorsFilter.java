4cd2dd6e1835318a60fadb6b484e8ab143bbdc9b
SeungpilPark <sppark@uengine.org>
2017-04-19 15:53:02 +0900

    authorization cors

--- iam-web/src/main/java/org/opencloudengine/garuda/web/security/CorsFilter.java
+++ iam-web/src/main/java/org/opencloudengine/garuda/web/security/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter{
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept, " +
-                "management-key, management-secret, client-key, client-secret");
+                "management-key, management-secret, client-key, client-secret, authorization");
         chain.doFilter(req, res);
     }
 
